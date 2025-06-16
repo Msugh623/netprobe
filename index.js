26f90f07e8dc2579d1e9d4cc6b3c4433a81805e2
Ernest Chia <iternenge469@gmail.com>
2025-06-16 20:12:46 +0100

    modified live checker'

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const os = require("os");
 
 class NetworkProbe {
   /**
-   * @param {function} callback - A callback function to be called after the network interface is detected, it is triggered by the autoDetect method
+   * @param {function} callback - A callback function to be called after the network interface is detected
    * @param {number} port - A port number to check if the network interface is live
    * @param {function} fallback - A callback function to be called if the network interface is not live
-   * @param {boolean} verbose - A boolean to enable verbose logging
+   * @param {boolean} v - A boolean to enable verbose logging
    * @returns {object} - Returns the network prober object
    * @description - A class to auto detect the network interface and check if it is live
    * @example
@@ -16,8 +16,8 @@ class NetworkProbe {
    * console.log(netFace)
    * // Output: { address: '', netmask: '', family: 'IPv4', mac: '', internal: false }
    */
-  constructor(port, callback = () => {}, verbose = false, fallback) {
-    this.verbose = verbose;
+  constructor(port = 3000, callback = () => {}, v = false, fallback) {
+    this.verbose = v;
     this.port = port;
     this.networkInterfaces = os.networkInterfaces();
     this.interfaceNames = Object.keys(this.networkInterfaces);
@@ -25,30 +25,42 @@ class NetworkProbe {
     this.callback = callback || (() => {});
     this.fallback = fallback || (() => {});
     this.heartbeat = true;
+    this.preference = "enp";
   }
 
-  initLiveCheck = (span = 5000) => {
+  prefer = (face = "end") => {
+    if (typeof face == "string") {
+      this.preference = face;
+      return
+    }
+    throw new TypeError('face parameter must be a string ')
+  };
+
+  initLiveCheck = () => {
     this.stopLiveCheck();
+    const port = this.port;
     this.liveInterval = setInterval(() => {
-      this.liveCheck(this.port, (err, live) => {
+      this.liveCheck(port, (err, live) => {
         if (err) {
           this.heartbeat = false;
           this.fallback();
-          console.error(err);
-          console.log(
-            `NetProbe: Network is offline... No fallback supplied. Retrying heartbeat in 5 seconds\n`
-          );
+          this.verbose && console.error(err);
+          this.verbose &&
+            console.log(
+              `NetProbe: Network is offline... No fallback supplied. Retrying heartbeat in 5 seconds\n`
+            );
         }
         if (live) {
           if (!this.heartbeat) {
-            console.log(
-              `\nNetProbe: Network is back online @ ${this.getUrl()}`
-            );
+            this.verbose &&
+              console.log(
+                `\nNetProbe: Network is back online @ http://${this.netface.address}:${port}`
+              );
             this.heartbeat = true;
           }
         }
       });
-    }, span);
+    }, 5000);
   };
 
   stopLiveCheck = () => {
@@ -73,12 +85,6 @@ class NetworkProbe {
     return (this.networkInterfaces["lo"] || []).find((network) =>
       this.isIpAddr(network.address)
     );
-  }
-
-  getUrl() {
-    return `${this.protocol || "http"}://${this.netface.address}${
-      this.port ? ":" : ""
-    }${this.port || ""}`;
   }
 
   autoDetect() {
@@ -109,8 +115,7 @@ class NetworkProbe {
       );
       faceNames = this.interfaceNames.filter((face) => !face.includes(eth));
       this.netface = theNetwork;
-      this.callback(this.netface);
-      return theNetwork
+      return theNetwork;
     }
 
     // No ETH use other Network Preference
@@ -130,7 +135,6 @@ class NetworkProbe {
         );
       const lo = this.getLocalNetwork();
       this.netface = lo;
-      this.callback(this.netface);
       return lo;
     }
 
@@ -146,54 +150,29 @@ class NetworkProbe {
         `NetProbe: Found a wireless IPv4 network... Using ${othernetFace.address} from interface ${othernetFace.netface} as the preferred network`
       );
     this.netface = othernetFace;
-    this.callback(this.netface);
     return othernetFace;
   }
 
-  liveCheck(cb = (err = Error, live = false) => {}, verbose = false) {
-    const url = this.getUrl();
-    axios
-      .head(url)
-      .then((_) => {
-        verbose && console.log(`NetProbe: ${url} is live`);
-        cb(null, true);
-      })
-      .catch((_) => {
-        const err = `NetProbeLiveCheck: !Faliure... ${url} Heartbeat failed DT: ${new Date()}`;
-        cb(err, false);
-        verbose && console.error(err);
-      });
-  }
-
-  chport() {
-    return this.port + 1;
-  }
-
-  async useSafePort() {
-    /** 
-      @returns {object} netface - The network interface object with the safe port
-      @description - A method to check if the current port is safe to use, if not it will change the port and return the new network interface object
-      @example
-      const netProb = new NetworkProbe(3000);
-      const netFace = await netProb.useSafePort();
-      console.log(netFace);
-
-      **/
-    const port = this.port;
-    const url = this.getUrl();
-    console.log(url);
+  async liveCheck(
+    port = this.port,
+    cb = (err = String(), live = false) => {
+      err;
+      live;
+    },
+    verbose = false
+  ) {
+    const netFace = this.netface;
     try {
-      const _ = await fetch(url, { method: "HEAD" });
-      this.verbose &&
-        console.log(
-          `EADDRINUSE: failed to use port ${port} as address is already in use... attempting change port`
-        );
-      this.port = this.chport(port);
-      return this.useSafePort(this.chport());
-    } catch (err) {
-      //  console.log(err.message)
-      this.port = port;
-      return { ...this.netface, port: this.port };
+      await axios.head(`http://${netFace.address}:${port}`);
+      verbose &&
+        console.log(`NetProbe: http://${netFace.address}:${port} is live`);
+      cb(null, true);
+    } catch (error) {
+      const err = `NetProbeLiveCheck: !Faliure... http://${
+        netFace.address
+      }:${port} Heartbeat failed DT: ${new Date()}`;
+      cb(err, false);
+      verbose && console.error(err);
     }
   }
 }
